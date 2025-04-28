d8b879ff37c09e56bd5f9672d3125c445a02d858
TwoSquirrels <RisuRisu.TwoSquirrels@gmail.com>
2025-04-29 02:51:45 +0900

    initialize Paper plugin

---
+++ src/main/kotlin/jp/trap/traQraft/TraQraft.kt
@@ -0,0 +1,14 @@
+package jp.trap.traQraft
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class TraQraft : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
